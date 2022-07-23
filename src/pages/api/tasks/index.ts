ff16826b63e73de31929259e074d0af5b9b8b4b2
Victor Hugo <victormotabatista@gmail.com>
2022-07-23 04:29:16 -0300

    Inserindo dados com as Tasks no banco

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import {conn} from '../../../utils/datebase';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req:NextApiRequest, res:NextApiResponse) => {
+export default async (req:NextApiRequest, res:NextApiResponse) => {
 
-    const {method} = req;
+    const {method, body} = req;
 
     switch (method) {
         case "GET":
@@ -11,6 +12,15 @@ export default (req:NextApiRequest, res:NextApiResponse) => {
         break;
 
         case "POST":
+            const {ID_COL, NOME, IDADE, SEXO, CPF, CID, UF, TELE_COL, EMAIL, SENHA, DEPA, GRUPO, STATUS, R_SOCIAL, DESCR} = body;
+            
+            const query = 'INSERT INTO WA_COLABORADOR(ID_COL, NOME, IDADE, SEXO, CPF, CID, UF, TELE_COL, EMAIL, SENHA, DEPA, GRUPO, STATUS, R_SOCIAL, DESCR) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *'
+            const values = [ID_COL, NOME, IDADE, SEXO, CPF, CID, UF, TELE_COL, EMAIL, SENHA, DEPA, GRUPO, STATUS, R_SOCIAL, DESCR]
+
+            const response = await conn.query(query, values)
+
+            console.log(response);
+           
             res.status(200).json("crating tasks");
         break;
 

b75b2a848e77a02f18e2f72ec1509239fa32d806
Victor Hugo <victormotabatista@gmail.com>
2022-07-23 01:50:32 -0300

    Atualização Tasks

---
+++ src/pages/api/tasks/[id].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse) => {
+
+    const {method} = req;
+
+    switch (method) {
+        case "GET":
+            res.json("getting a unique tasks");
+        break;
+
+        case "PUT":
+            res.json("updating a unique tasks");
+        break;
+
+        case "DELETE":
+            res.json("Deleting a unique task");
+
+        break;
+
+        default:
+            res.status(400).json("Method not allowed");
+    }
+}
